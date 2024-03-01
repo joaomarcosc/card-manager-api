46aefa8ac38d25a18624b0b8e1dbcd248aeece01
joaomarcos <j.marcos9@hotmail.com>
2024-03-01 09:39:13 -0300

    feat: enable cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import { Users } from './routes/users'
 import fastifyCookie from '@fastify/cookie'
 import { Auth } from './routes/auth'
 import { Cards } from './routes/cards'
+import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
 
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'PUT', 'POST', 'PATCH'],
+})
 app.register(fastifyCookie)
 app.register(Auth, {
   prefix: 'auth',
